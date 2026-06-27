1341c852cef345613412982fddb5a35755d4243f
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-27 21:25:26 +0530

    added insta

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           {/* Mobile photo - shows only on mobile when 3D character is hidden */}
           <div className="mobile-photo">
-            <img src="/images/mypicnbg.png" alt="Alokit Mishra" />
+            <img src="/images/mypicnbg.webp" alt="Alokit Mishra" loading="eager" decoding="async" />
           </div>
         </div>
         {children}
